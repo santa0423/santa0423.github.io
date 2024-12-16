9bb2995f613e32da2b6d570c8ff64c120d75a651
boeun0423 <sbe22134711@gmail.com>
2024-12-16 23:56:39 +0900

    x
    x

--- 취업(운세 페이지에서).js
+++ 취업(운세 페이지에서).js
@@ -1,22 +1,32 @@
+// 카드별로 다른 페이지로 이동하도록 설정
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', () => {
-    // 이동 가능한 페이지 목록
-    const pages = [
-'공사중.html'
-    ];
-
-    // 랜덤으로 페이지 선택
-    const randomIndex = Math.floor(Math.random() * pages.length);
-    const targetPage = pages[randomIndex];
-
-    // 랜덤으로 선택된 페이지로 이동
+    const cardId = card.id.split('-')[1];
+    
+    // 각 카드별로 이동할 페이지 설정
+    let targetPage = '';
+    switch (cardId) {
+      case '1':
+        targetPage = 'page1(취업).html'; // 카드 1 클릭 시 page1.html로 이동
+        break;
+      case '2':
+        targetPage = 'page2.html'; // 카드 2 클릭 시 page2.html로 이동
+        break;
+      case '3':
+        targetPage = 'page3.html'; // 카드 3 클릭 시 page3.html로 이동
+        break;
+      case '4':
+        targetPage = 'page4.html'; // 카드 4 클릭 시 page4.html로 이동
+        break;
+      case '5':
+        targetPage = 'page5.html'; // 카드 5 클릭 시 page5.html로 이동
+        break;
+      default:
+        targetPage = 'defaultPage.html'; // 기본 페이지
+        break;
+    }
+    
+    // 지정된 페이지로 이동
     window.location.href = targetPage;
   });
 });
-//document.querySelectorAll('.card').forEach(card => {  card.addEventListener('click', () => {    // 이동 가능한 페이지 목록const pages = ['재물1.html',   '재물2.html',    '재물3.html',     '재물4.html',     '재물5.html'   ];    // 랜덤으로 페이지 선택   const randomIndex = Math.floor(Math.random() * pages.length);
-//    const targetPage = pages[randomIndex];
-
-    // 랜덤으로 선택된 페이지로 이동
-  //  window.location.href = targetPage;
- // });
-//});
